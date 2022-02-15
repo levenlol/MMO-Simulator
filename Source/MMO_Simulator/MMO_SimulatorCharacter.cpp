2732540a1f7a0a4b2adeb7214bd5f99449764622
Paolo Bernacca <bernaccapaolo@gmail.com>
2022-02-15 12:47:01 +0100

    Basic folder structure.
    Import new assets and convert to ue5

--- Source/MMO_Simulator/MMO_SimulatorCharacter.cpp
+++ Source/MMO_Simulator/MMO_SimulatorCharacter.cpp
@@ -44,7 +44,7 @@ AMMO_SimulatorCharacter::AMMO_SimulatorCharacter()
 	// Create a decal in the world to show the cursor's location
 	CursorToWorld = CreateDefaultSubobject<UDecalComponent>("CursorToWorld");
 	CursorToWorld->SetupAttachment(RootComponent);
-	static ConstructorHelpers::FObjectFinder<UMaterial> DecalMaterialAsset(TEXT("Material'/Game/TopDownCPP/Blueprints/M_Cursor_Decal.M_Cursor_Decal'"));
+	static ConstructorHelpers::FObjectFinder<UMaterial> DecalMaterialAsset(TEXT("Material'/Game/Materials/Hud/M_Cursor_Decal.M_Cursor_Decal'"));
 	if (DecalMaterialAsset.Succeeded())
 	{
 		CursorToWorld->SetDecalMaterial(DecalMaterialAsset.Object);
--- Source/MMO_Simulator/MMO_SimulatorGameMode.cpp
+++ Source/MMO_Simulator/MMO_SimulatorGameMode.cpp
@@ -9,11 +9,4 @@ AMMO_SimulatorGameMode::AMMO_SimulatorGameMode()
 {
 	// use our custom PlayerController class
 	PlayerControllerClass = AMMO_SimulatorPlayerController::StaticClass();
-
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
-	if (PlayerPawnBPClass.Class != nullptr)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
 }
